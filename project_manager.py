083f576238ff1cc3a6a5fd37b91323ec97079e53
morrolinux <morrolinux@gmail.com>
2019-11-30 10:32:02 +0100

    pretty much works but needs keyframe alignment and a proper cleanup refractor

--- project_manager.py
+++ project_manager.py
@@ -10,7 +10,7 @@ class Job:
         self.export_name = "video"
         self.len = job_weight
         self.split = split
-        self.last_frame = 0
+        self.last_rendered_frame = 0
 
     def __str__(self):
         return "" + self.job_path + " : " + str(self.job_weight)
@@ -41,6 +41,7 @@ class ProjectManager:
         self.split_job = None
         self.split_nodes = []
         self.split_nodes_done = 0
+        self.parts = []
 
     def explore(self, folder):
         for root, dirs, files in os.walk(folder):
@@ -58,15 +59,30 @@ class ProjectManager:
         num_clips = len(items)
         return max(int(v.attributes['out'].value) for v in items[max(0, num_clips - 50):num_clips])
 
+    def write_concat_list(self, list_name):
+        self.parts.sort()
+        with open(list_name, "w") as m:
+            for p in self.parts:
+                m.write("file \'" + str(p) + ".mp4\'\n")
+
+    def merge_parts(self, output_name):
+        os.chdir(self.split_job.job_path[:self.split_job.job_path.rfind("/")])
+        list_name = "merge.txt"
+        self.write_concat_list(list_name)
+        os.system("ffmpeg -f concat -safe 0 -i " + list_name + " -c copy " + output_name + ".mp4")
+        os.remove(list_name)
+        for p in self.parts:
+            os.remove(str(p) + ".mp4")
+
     def get_job(self, n):
         # when each node has come back asking for a job, we know they all finished the ongoing split job
         if n in self.split_nodes:
             self.split_nodes_done = self.split_nodes_done + 1
             if self.split_nodes_done == len(self.render_nodes):
-                # TODO: cleanup tar archive for the RESPECTIVE JOB (atm only one job is possible)
-                print("rm", self.split_job.job_path[:self.split_job.job_path.rfind("/")]+".tar")
-                # TODO: merge the partial files
-                pass
+                # TODO: ATM only one split job per invocation is possible
+                os.remove(self.split_job.job_path[:self.split_job.job_path.rfind("/")]+".tar")
+                self.merge_parts(self.split_job.job_path[self.split_job.job_path.rfind("/")+1:])
+                print("Export merged. Finished!!!")
 
         if len(self.jobs) <= 0:
             print("PROJECT MANAGER: no more work to do")
@@ -104,34 +120,33 @@ class ProjectManager:
                     return Job("abort", -1), None, None
 
         if assigned_job.split:
+            self.split_job = assigned_job
 
             # assign the given worker a chunk size proportional to its rank
             tot_workers_score = 0
             for worker in self.render_nodes:
                 tot_workers_score = tot_workers_score + worker.cpu_score
-            chunk_size = math.ceil((n.cpu_score / tot_workers_score) * assigned_job.len)
+            chunk_size = math.ceil((n.cpu_score / tot_workers_score) * self.split_job.len)
 
             # where to start/end the chunk (and update seek)
-            job_start = assigned_job.last_frame
-            job_end = min(job_start + chunk_size, assigned_job.len)
-            assigned_job.last_frame = job_end
+            job_start = self.split_job.last_rendered_frame
+            job_end = min(job_start + chunk_size, self.split_job.len)
+            # TODO: Make sure to ALWAYS end on a keyframe...
+            self.split_job.last_rendered_frame = job_end  # + 1
 
             # append the given worker to the list of workers doing a split job; save the split job
             # TODO: beware of race condition for len(self.split_nodes)
             self.split_nodes.append(n)
-            self.split_job = assigned_job
-            self.split_job.export_name = len(self.split_nodes)
+            self.parts.append(len(self.split_nodes))
+            self.split_job.export_name = self.parts[-1]
 
-            print(n.address, "will export", assigned_job.job_path, "from", job_start, "to", job_end)
+            print(n.address, "will export", self.split_job.job_path, "from", job_start, "to", job_end)
 
-            if assigned_job.len == job_end:
+            if self.split_job.len == job_end:
                 self.jobs.remove(assigned_job)
-                # TODO: assegnare un nome sequenziale ai progetti da esportare per poterli mergiare con ffmpeg
-                # TODO: quando ciascun nodo che ha preso parte ad un job split chiede un nuovo lavoro, so di aver
-                #  terminato e posso ripulire dall'archivio .tar e UNIRE i file parziali rientrati dai nodi
-                # TODO: probabilmente posso evitare di salvare self.split job ed evitare di eliminarlo dalla coda
-                #  qui, eliminandolo al verificarsi della condizione self.split_nodes_done == len(self.render_nodes)
-            return assigned_job, job_start, job_end
+                # TODO: probably don't need to save self.split and remove it from the queue, but instead
+                #  keep it until termination condition (at the beginning of this procedure) is reached.
+            return self.split_job, job_start, job_end
 
         self.jobs.remove(assigned_job)
-        return assigned_job, None, None
+        return self.split_job, None, None
